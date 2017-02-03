c37888dacae49a64f2292fbbe8fcc3c96e1e71f9
jeongmincha <jeongmin.cha@kaist.ac.kr>
2017-02-03 18:02:23 +0900

    Add test script for using test suite

--- run-test.py
+++ run-test.py
@@ -1,1 +1,4 @@
+import unittest
 import test.test_time_series_json
+
+unittest.TextTestRunner().run(test.test_time_series_json.suite)
--- test/test_time_series_json.py
+++ test/test_time_series_json.py
@@ -7,8 +7,8 @@ test_json = TimeSeriesJSON(fn='test/test_json.json')
 class TestTimeSeriesJSON(unittest.TestCase):
 
     def test_parse_json_items(self):
-        items = self.test_json.parse_json_items()
-        self.assertTrue(len(items), 9)
+        self.assertTrue(True)
+        items = test_json.parse_json_items(tag='item')
+        self.assertEqual(len(items), 9)
 
-if __name__ == '__main__':
-    unittest.main()
+suite = unittest.TestLoader().loadTestsFromTestCase(TestTimeSeriesJSON)
