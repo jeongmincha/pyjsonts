1de2511bcc73a2bab67372a7835ce9aa7a671963
jeongmincha <jeongmin.cha@kaist.ac.kr>
2017-02-03 14:33:15 +0900

    Make constructors of TimeSeriesJSON class
    Now we can construct instances of TimeSeriesJSON with two options file
    or file name.

--- pyjsonts/time_series_json.py
+++ pyjsonts/time_series_json.py
@@ -1,3 +1,14 @@
 class TimeSeriesJSON:
-    def __init__(self):
-        pass
+
+    def __init__(self, f=None, fn=None):
+        """
+        :param f: file object (_io.TextIOWrapper)
+        :param fn: file name as a string
+        """
+        if f is not None:
+            self.__type = 'file'
+            self.__file = f
+        elif fn is not None:
+            self.__type = 'file_name'
+            self.__file_name = fn
+            self.__file = open(fn)
